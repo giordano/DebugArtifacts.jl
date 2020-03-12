9c9e53cdb969db5e1e26da1579b37cecaf99e9a4
Mosè Giordano <mose@gnu.org>
2020-03-12 23:01:19 +0000

    Files generated by PkgTemplates

---
+++ src/DebugArtifacts.jl
@@ -0,0 +1,5 @@
+module DebugArtifacts
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DebugArtifacts
+using Test
+
+@testset "DebugArtifacts.jl" begin
+    # Write your own tests here.
+end
